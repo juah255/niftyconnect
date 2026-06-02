fe951fe85d1d6ecbae9fb921bd3bafb25f7b104b
juah255 <junayedahammed94@gmail.com>
2026-06-02 10:55:54 +0600

    exclude the source code

--- build/admin.asset.php
+++ build/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '3b8b3433eaf76e76e9a3');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-api-fetch', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => 'f9a4ba9c7bb4ca663a05');
